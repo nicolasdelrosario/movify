40f8a8ee5e3aa118ca870ac0e986800036fa0595
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-18 22:31:08 -0500

    feat: :sparkles: add buttons at the navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,26 +1,28 @@
 import { Clapperboard } from "lucide-react";
 import MaxWidthWrapper from "../max-width-wrapper";
+import ThemeToggle from "../theme-toggle";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 
-const Navbar = () => {
+function Navbar() {
 	return (
-		<div className="border-gray-200 border-b ">
+		<div className="border-gray-200 border-b dark:border-gray-700 ">
 			<MaxWidthWrapper className="py-2">
 				<div className="flex items-center justify-between">
 					<div className="flex items-center">
 						<Clapperboard className="mr-2 font-semibold" />
 						<h1 className="font-semibold text-xl">Movify</h1>
 					</div>
-					<div>
-						<Button size={"sm"}>Registrarse</Button>
-						<Button variant={"outline"} size={"sm"} className="ml-2">
-							Iniciar sesión
-						</Button>
+					<div className="flex h-8 items-center space-x-4 text-sm">
+						<Button>Registrarse</Button>
+						<Button variant={"outline"}>Iniciar sesión</Button>
+						<Separator orientation="vertical" />
+						<ThemeToggle />
 					</div>
 				</div>
 			</MaxWidthWrapper>
 		</div>
 	);
-};
+}
 
 export default Navbar;
