81732bdff6110d11f31116a1f0c10280f1e7dbca
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-18 22:30:42 -0500

    feat: :sparkles: applying new providers

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,9 +1,18 @@
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import ReactScanProvider from "@/components/providers/react-scan-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { env } from "@/config/env";
 
-function Providers() {
+type ProvidersProps = {
+	children: React.ReactNode;
+};
+
+function Providers({ children }: ProvidersProps) {
 	return (
 		<>
-			<TanStackRouterDevtools />
+			<ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+				{env.NODE_ENV === "development" && <ReactScanProvider />}
+				{children}
+			</ThemeProvider>
 		</>
 	);
 }
